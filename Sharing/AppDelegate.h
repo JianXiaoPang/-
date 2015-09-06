97d222a65396c05505ad71be782942b657187016
黄承琪 <huangchengqi@huangchengqideMacBook-Pro.local>
2015-09-06 22:00:00 +0800

    Initial Commit

---
+++ Sharing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sharing
+//
+//  Created by 黄承琪 on 15/9/6.
+//  Copyright (c) 2015年 黄承琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sharing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sharing
+//
+//  Created by 黄承琪 on 15/9/6.
+//  Copyright (c) 2015年 黄承琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
